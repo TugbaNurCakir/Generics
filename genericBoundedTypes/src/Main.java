aa395a684cbeb1302bcaf8716cd7529fa881d2e0
tugbanur <nurtugba2110@gmail.com>
2021-10-13 22:21:13 +0300

    boundedTypes

---
+++ genericBoundedTypes/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    /*
+    örnek classım
+        public class Nullable<T extends Number>
+
+    extends deyimini kullandıysak yalnızca belirttiğimiz türü veya alt sınıflarını kullanabiliriz. Buna üst sınır (upper-bound) denir.
+     super deyimini kullandıysak yalnızca belirttiğimiz türün üst sınıflarını kullanabiliriz. Buna alt sınır (lower-bound) denir.
+
+Nullable<Integer> nullableInteger = new Nullable<Integer>(2020);
+   Bu kullanım uygundur; çünkü Integer, Number sınıfından türetilmiştir
+Nullable<String> nullableString = new Nullable<String>("2020");
+ Bu kullanım uygun değildir; String, Number sınıfından türetilmemiştir
+     */
+}
+
